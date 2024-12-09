2efb7904d92732a6de9696ba50b73371d941c8d3
Manish Meganathan <manish@sarva.ai>
2024-12-10 01:44:44 +0530

    extend common utilities
    - add a general Nil type for any 32 byte nil value
    - remove has0xPrefixString as it will no longer be used. All identifiers will be 32-byte array values.
    - add trim32, trimLow4, trimHigh4, trimMid24 for byte array manipulation
    - add isBitSet to check if a bit is set in a set of flags (byte)
    - add general marshal32 and unmarshal32 functions to handle json encoding/decoding

--- common.go
+++ common.go
@@ -7,17 +7,23 @@ import (
 	"strings"
 )
 
+// Nil is a nil [32]byte value.
+// Can be used to represent any nil identifier.
+var Nil [32]byte
+
 var (
 	prefix0xString = "0x"
 	prefix0xBytes  = []byte(prefix0xString)
 )
 
+var (
+	ErrMissingHexPrefix = errors.New("missing '0x' prefix")
+	ErrInvalidLength    = errors.New("invalid length")
+	ErrUnsupportedFlag  = errors.New("unsupported flag")
+)
+
 func trim0xPrefixString(value string) string {
 	return strings.TrimPrefix(value, prefix0xString)
-}
-
-func has0xPrefixString(value string) bool {
-	return strings.HasPrefix(value, prefix0xString)
 }
 
 func trim0xPrefixBytes(value []byte) []byte {
@@ -27,11 +33,6 @@ func trim0xPrefixBytes(value []byte) []byte {
 func has0xPrefixBytes(value []byte) bool {
 	return bytes.HasPrefix(value, prefix0xBytes)
 }
-
-var (
-	ErrMissing0xPrefix = errors.New("missing '0x' prefix")
-	ErrInvalidLength   = errors.New("invalid length")
-)
 
 func decodeHexString(str string) ([]byte, error) {
 	// Trim the 0x prefix from the string (if it exists)
@@ -43,6 +44,76 @@ func decodeHexString(str string) ([]byte, error) {
 	}
 
 	return decoded, nil
+}
+
+func trim32(data []byte) (trim [32]byte) {
+	// Trim the data if it is longer than 32 bytes
+	if len(data) > 32 {
+		data = data[len(data)-32:]
+	}
+
+	// Copy the data into the trimmed array
+	// This automatically pads the array if the given data is less than 32 bytes
+	copy(trim[32-len(data):], data)
+
+	return
+}
+
+func trimHigh4(bytes [32]byte) [4]byte {
+	return [4]byte(bytes[:4])
+}
+
+func trimMid24(bytes [32]byte) [24]byte {
+	return [24]byte(bytes[4:28])
+}
+
+func trimLow4(bytes [32]byte) [4]byte {
+	return [4]byte(bytes[28:])
+}
+
+func isBitSet(value byte, loc uint8) bool {
+	if loc > 7 {
+		panic("invalid flag location: must be between 0 and 7")
+	}
+
+	// Determine the bit value at the given location
+	bit := (value >> loc) & 0x1
+	// Check if bit is set
+	return bit != 0
+}
+
+func marshal32(data [32]byte) ([]byte, error) {
+	buffer := make([]byte, 32*2+2)
+
+	// Copy the 0x prefix into the buffer
+	copy(buffer[:2], prefix0xString)
+	// Hex-encode the copied value into the buffer
+	hex.Encode(buffer[2:], data[:])
+
+	return buffer, nil
+}
+
+func unmarshal32(data []byte) ([32]byte, error) {
+	// Assert that the 0x prefix exists
+	if !has0xPrefixBytes(data) {
+		return Nil, ErrMissingHexPrefix
+	}
+
+	// Trim the 0x prefix
+	data = trim0xPrefixBytes(data)
+
+	// Check that the data has enough length for the identifier data
+	if len(data) != 32*2 {
+		return Nil, ErrInvalidLength
+	}
+
+	// Decode the hex-encoded data
+	decoded, err := decodeHexString(string(data))
+	if err != nil {
+		return Nil, err
+	}
+
+	return [32]byte(decoded), nil
 }
 
 func must[T any](t T, err error) T {
