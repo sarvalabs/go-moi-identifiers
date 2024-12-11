686685b751ae53a48b81b86ddb6f5c6a4338b57f
Manish Meganathan <manish@sarva.ai>
2024-12-11 19:45:22 +0530

    implement better flag handling
    - add Flag type for universal flag specifier
    - add flag management code and centralized flag support checks and mask generation
    - refactor flags for asset and logic ID
    - add systemic flag in the 7th bit for all identifiers

--- common.go
+++ common.go
@@ -32,18 +32,23 @@ var (
 	ErrUnsupportedKind    = errors.New("unsupported tag kind")
 )
 
+// trim0xPrefixString trims the 0x prefix from the given string (if it exists).
 func trim0xPrefixString(value string) string {
 	return strings.TrimPrefix(value, prefix0xString)
 }
 
+// trim0xPrefixBytes trims the 0x prefix from the given byte slice (if it exists).
 func trim0xPrefixBytes(value []byte) []byte {
 	return bytes.TrimPrefix(value, prefix0xBytes)
 }
 
+// has0xPrefixBytes checks if the given byte slice has a 0x prefix.
 func has0xPrefixBytes(value []byte) bool {
 	return bytes.HasPrefix(value, prefix0xBytes)
 }
 
+// decodeHexString decodes the given hex string into a byte slice.
+// It trims the 0x prefix (if found) from the string before decoding.
 func decodeHexString(str string) ([]byte, error) {
 	// Trim the 0x prefix from the string (if it exists)
 	str = trim0xPrefixString(str)
@@ -56,6 +61,7 @@ func decodeHexString(str string) ([]byte, error) {
 	return decoded, nil
 }
 
+// trim32 trims/pads the given byte slice to 32 bytes
 func trim32(data []byte) (trim [32]byte) {
 	// Trim the data if it is longer than 32 bytes
 	if len(data) > 32 {
@@ -69,29 +75,23 @@ func trim32(data []byte) (trim [32]byte) {
 	return
 }
 
+// trimHigh4 returns the 4 most-significant bytes of the given 32-byte array.
 func trimHigh4(bytes [32]byte) [4]byte {
 	return [4]byte(bytes[:4])
 }
 
+// trimMid24 returns the 24 bytes in the middle of the given 32-byte array.
 func trimMid24(bytes [32]byte) [24]byte {
 	return [24]byte(bytes[4:28])
 }
 
+// trimLow4 returns the 4 least-significant bytes of the given 32-byte array.
 func trimLow4(bytes [32]byte) [4]byte {
 	return [4]byte(bytes[28:])
 }
 
-func isBitSet(value byte, loc uint8) bool {
-	if loc > 7 {
-		panic("invalid flag location: must be between 0 and 7")
-	}
-
-	// Determine the bit value at the given location
-	bit := (value >> loc) & 0x1
-	// Check if bit is set
-	return bit != 0
-}
-
+// marshal32 is a generic marshal function for 32-byte identifiers.
+// To be used in conjunction with MarshalText
 func marshal32(data [32]byte) ([]byte, error) {
 	buffer := make([]byte, 32*2+2)
 
@@ -103,6 +103,8 @@ func marshal32(data [32]byte) ([]byte, error) {
 	return buffer, nil
 }
 
+// unmarshal32 is generic unmarshal function for 32-byte identifiers.
+// To be used in conjunction with UnmarshalText
 func unmarshal32(data []byte) ([32]byte, error) {
 	// Assert that the 0x prefix exists
 	if !has0xPrefixBytes(data) {
@@ -126,6 +128,9 @@ func unmarshal32(data []byte) ([32]byte, error) {
 	return [32]byte(decoded), nil
 }
 
+// must is correctness enforcer for error handling.
+// For use in functions that should never return an error.
+// Panics if an error is encountered.
 func must[T any](t T, err error) T {
 	if err != nil {
 		panic(err)
---
+++ flags.go
@@ -0,0 +1,107 @@
+package identifiers
+
+var (
+	// Systemic is a Flag for the 7th flag bit on all identifiers regardless of the kind.
+	// It indicates that the account associated with identifier belongs to the system.
+	// Supported from v0 for all identifiers
+	Systemic = Flag{
+		index: 7,
+		support: map[IdentifierKind]uint8{
+			KindParticipant: 0,
+			KindAsset:       0,
+			KindLogic:       0,
+		},
+	}
+
+	// AssetStateful is a Flag on AssetID for the Stateful flag on its 0th bit.
+	// It indicates that the asset has some stateful information such as its supply.
+	// Supported from v0 of AssetID
+	AssetStateful = makeFlag(KindAsset, 0, 0)
+	// AssetLogical is a Flag on AssetID for the Logical flag on its 1st bit.
+	// It indicates that the asset has some logic associated with it.
+	// Supported from v0 of AssetID
+	AssetLogical = makeFlag(KindAsset, 1, 0)
+
+	// LogicIntrinsic is a Flag on LogicID for the Intrinsic flag on its 0th bit.
+	// It indicates that the logic manages some intrinsic state
+	// Supported from v0 of LogicID
+	LogicIntrinsic = makeFlag(KindLogic, 0, 0)
+	// LogicExtrinsic is a Flag on LogicID for the Extrinsic flag on its 1st bit.
+	// It indicates that the logic manages some extrinsic state
+	// Supported from v0 of LogicID
+	LogicExtrinsic = makeFlag(KindLogic, 1, 0)
+	// LogicAuxiliary is a Flag on LogicID for the Auxiliary flag on its 2nd bit.
+	// It indicates that the logic is attached as an auxiliary to another object.
+	// Supported from v0 of LogicID
+	LogicAuxiliary = makeFlag(KindLogic, 2, 0)
+)
+
+// Flag represents a flag specifier for an identifier.
+type Flag struct {
+	// the bit index of the flag
+	index uint8
+	// the supported identifier kinds mapped to minimum supported version
+	support map[IdentifierKind]uint8
+}
+
+// Supports returns if the flag is supported by the given kind.
+func (flag Flag) Supports(tag IdentifierTag) bool {
+	// Check if the kind is supported by the flag & obtain version
+	version, ok := flag.support[tag.Kind()]
+	if !ok {
+		return false
+	}
+
+	// Check if the version is supported by flag
+	return tag.Version() >= version
+}
+
+// getFlag retrieves a flag value from a given flag set and an index.
+func getFlag(value byte, index uint8) bool {
+	// Determine the bit value at the given index
+	bit := (value >> index) & 0x1
+	// Check if flag is set
+	return bit != 0
+}
+
+// setFlag updates the flag value for a given flag
+// set and index and returns the modified flag set.
+//
+// Whether to set/unset the flag is determined by the flag input.
+func setFlag(value byte, index uint8, flag bool) byte {
+	if flag {
+		value |= 1 << index // Set the bit value at the given index
+	} else {
+		value &^= 1 << index // Unset the bit value at the given index
+	}
+
+	return value
+}
+
+// makeFlag is used to construct a valid Flag object
+// which is only supported by a single IdentifierKind
+func makeFlag(kind IdentifierKind, index uint8, version uint8) Flag {
+	if index > 7 {
+		panic("invalid flag location: must be between 0 and 7")
+	}
+
+	if version > 7 {
+		panic("invalid flag version: must be between 0 and 7")
+	}
+
+	return Flag{
+		index:   index,
+		support: map[IdentifierKind]uint8{kind: version},
+	}
+}
+
+// flagMasks represent the mask of supported flags for an IdentifierTag.
+// Can be accessed with IdentifierTag.FlagMask().
+//
+// A set bit indicates that position is not allowed for the tag,
+// While an unset bit indicates it is a supported flag for the tag.
+var flagMasks = map[IdentifierTag]byte{
+	TagParticipantV0: 0b11111110,
+	TagLogicV0:       0b00011110,
+	TagAssetV0:       0b00111110,
+}
